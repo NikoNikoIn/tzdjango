41b90c488175e500512ccfc57326984b2656dc02
NikoNikoIn <legacytild@gmail.com>
2023-09-26 01:23:59 +0300

    Added the getLessonsByProduct view and its url

--- tzdjango/tzmodels/urls.py
+++ tzdjango/tzmodels/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('lessons/', getLessonList),
     path('userlessons/', getUserLessons),
+    path('productlessons/<str:pk>/', getLessonsByProduct),
 ]
--- tzdjango/tzmodels/views.py
+++ tzdjango/tzmodels/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import generics
+from rest_framework import status
 
 from .models import *
 from .serializers import *
@@ -15,11 +15,29 @@ def getLessonList(request):
     return Response(serializer.data)
 
 
+#1
 @api_view(['GET'])
 def getUserLessons(request):
     user = request.user
-    lessons = Lesson.objects.filter(inProduct__owner=user)
-    serializer = LessonSerializer(lessons, many=True)
-    views = LessonView.objects.filter(user=user)
+    lessons = Lesson.objects.filter(inProduct__owner = user)
+    serializer = LessonSerializer(lessons, many = True)
+    views = LessonView.objects.filter(user = user)
     serializerViews = LessonViewSerializer(views, many = True)
-    return Response({'lessons': serializer.data, 'products': serializerViews.data})
+    return Response({'lessons': serializer.data, 'views': serializerViews.data})
+
+
+#2
+@api_view(['GET'])
+def getLessonsByProduct(request, pk):
+    user = request.user
+
+    try:
+        product = Product.objects.get(name=pk)
+        lessons = Lesson.objects.filter(inProduct__name = product, inProduct__owner = user)
+        views = LessonView.objects.filter(user = user, lesson__in = lessons)
+        serializer = LessonSerializer(lessons, many = True)
+        serializerViews = LessonViewSerializer(views, many = True)
+        return Response({'lessons': serializer.data, 'views': serializerViews.data})
+    except:
+        return Response({'detail':'Product does not exist or you are not authorized'}, status=status.HTTP_400_BAD_REQUEST)
+        
